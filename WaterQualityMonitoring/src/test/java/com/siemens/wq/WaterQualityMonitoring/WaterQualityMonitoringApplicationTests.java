e308fe3bad4f316983b6aea3bda32a20ee929873
z003tcny <z003tcny@md1tkvfc.ad001.siemens.net>
2020-03-05 19:34:54 +0530

    Test file

---
+++ WaterQualityMonitoring/src/test/java/com/siemens/wq/WaterQualityMonitoring/WaterQualityMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siemens.wq.WaterQualityMonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterQualityMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
